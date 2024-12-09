90793e3eb403f6113ab19af104542eb54cef4092
StynlFear <gula_roby2@yahoo.com>
2024-12-09 19:25:01 +0200

    BP

--- src/pages/ExamsList.jsx
+++ src/pages/ExamsList.jsx
@@ -3,16 +3,23 @@ import '../Dashboard.css';
 import { HiMiniMagnifyingGlass, HiPlus } from "react-icons/hi2";
 import axios from 'axios';
 import Sidebar from '../components/Sidebar';
-import ExamDropdown from '../components/ExamDropDown'; // Import the new ExamDropdown component
-import {jwtDecode} from 'jwt-decode'; // Import jwt-decode to decode the token
+import ExamDropdown from '../components/ExamDropDown';
+import ReactPaginate from 'react-paginate';
+import { jwtDecode } from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const backendURL = import.meta.env.VITE_BACKEND_URL;
 
 const Dashboard = () => {
   const [exams, setExams] = useState([]);
   const [userRole, setUserRole] = useState(null);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(1);
+  const [searchTerm, setSearchTerm] = useState('');
+  const examsPerPage = 5;
+  const navigate = useNavigate();
 
-  // Function to decode the token and get the role
   const getUserRole = () => {
     const token = localStorage.getItem('token');
     if (!token) return null;
@@ -38,32 +45,55 @@ const Dashboard = () => {
     return null;
   };
 
-  // Set user role when the component mounts
   useEffect(() => {
     setUserRole(getUserRole());
-  }, []);
-
-  // Fetch exams data when the component mounts
-  useEffect(() => {
     const fetchExams = async () => {
       try {
         const response = await axios.get(`${backendURL}/exam`);
-        setExams(response.data.exams);
+        const allExams = response.data.exams;
+  
+        // Filter exams based on the search term for subject, group, classroom, examiner, and hour
+        const filteredExams = allExams.filter(exam => 
+          exam.subject.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          (exam.group && exam.group.name.toLowerCase().includes(searchTerm.toLowerCase())) ||
+          (exam.mainProfessor && `${exam.mainProfessor.firstName} ${exam.mainProfessor.lastName}`.toLowerCase().includes(searchTerm.toLowerCase())) ||
+          (exam.secondaryProfessor && `${exam.secondaryProfessor.firstName} ${exam.secondaryProfessor.lastName}`.toLowerCase().includes(searchTerm.toLowerCase())) ||
+          (exam.classroom && exam.classroom.name.toLowerCase().includes(searchTerm.toLowerCase())) ||
+          (exam.hour.toLowerCase().includes(searchTerm.toLowerCase()))||
+          (new Date(exam.date).toLocaleDateString().toLowerCase().includes(searchTerm.toLowerCase()))
+        );
+  
+        // Calculate total pages
+        setTotalPages(Math.ceil(filteredExams.length / examsPerPage));
+  
+        // Set the exams for the current page
+        const startIndex = currentPage * examsPerPage;
+        const currentPageExams = filteredExams.slice(startIndex, startIndex + examsPerPage);
+        setExams(currentPageExams);
+  
       } catch (error) {
         console.error('Error fetching exams:', error);
       }
     };
-
+  
     fetchExams();
-  }, []);
+  }, [currentPage, searchTerm]);
 
-  // Function to remove an exam from the state after deletion
   const removeExamFromState = (examId) => {
     setExams(exams.filter(exam => exam._id !== examId));
   };
 
+  const handlePageClick = (event) => {
+    setCurrentPage(event.selected);
+  };
+
   const handleItemClick = (path) => {
-    window.location.href = path;
+    navigate(path);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+    setCurrentPage(0); // Reset to the first page when search changes
   };
 
   return (
@@ -73,67 +103,88 @@ const Dashboard = () => {
         <section className="section exams">
           <div className="header-bar">
             <h3>Orar examene</h3>
-            {/* Render the "Create Exam" button only if the user is a professor */}
             {userRole === 'professor' && (
-              <button className="add-request-button" onClick={() => handleItemClick('/createexam')}>
-                <HiPlus size={24} />
-                AE
-              </button>
-            )}
-            {/* Render the "Create Class" button for all roles */}
-            {userRole === 'professor' && (
-            <button className="add-request-button" onClick={() => handleItemClick('/createclass')}>
-              <HiPlus size={24} />
-              AS
-            </button>
+              <>
+                <button className="add-request-button" onClick={() => handleItemClick('/createexam')}>
+                  <HiPlus size={24} />
+                  AE
+                </button>
+                <button className="add-request-button" onClick={() => handleItemClick('/createclass')}>
+                  <HiPlus size={24} />
+                  AS
+                </button>
+              </>
             )}
           </div>
 
           <div className="search-bar">
-            <input type="text" placeholder="Search..." />
-            <button type="submit" className="bg-white px-3 py-3">
+            <input 
+              type="text" 
+              placeholder="Search..." 
+              value={searchTerm} 
+              onChange={handleSearchChange} 
+              className="form-control" 
+            />
+            <button type="submit" className="btn btn-outline-primary">
               <HiMiniMagnifyingGlass />
             </button>
           </div>
 
-          {/* Table Headers (Columns) */}
           <div className="table-header">
-            <span>Materie</span>         {/* Column for Subject */}
-            <span>Grupa</span>          {/* Column for Group */}
-            <span>Examiner</span>       {/* Column for Main Professor */}
-            <span>Assistant</span>      {/* Column for Secondary Professor */}
-            <span>Data</span>           {/* Column for Exam Date */}
-            <span>Ora</span>            {/* Exam Hour */}
-            <span>Sala</span>           {/* Classroom */}
-            {userRole === 'professor' && (<span>Actiuni</span> )}       {/* Actions */}
+            <span>Materie</span>
+            <span>Grupa</span>
+            <span>Examiner</span>
+            <span>Assistant</span>
+            <span>Data</span>
+            <span>Ora</span>
+            <span>Sala</span>
+            {userRole === 'professor' && <span>Actiuni</span>}
           </div>
 
-          {/* Table Body (Rows) */}
           <div className="table-body">
             {exams.length > 0 ? (
               exams.map((exam) => (
                 <div key={exam._id} className="table-row">
-                  <span>{exam.subject}</span>          {/* Subject */}
-                  <span>{exam.group?.name || 'No group assigned'}</span>   {/* Group Name */}
-                  <span>{exam.mainProfessor?.firstName} {exam.mainProfessor?.lastName || 'N/A'}</span>  {/* Main Professor */}
+                  <span>{exam.subject}</span>
+                  <span>{exam.group?.name || 'No group assigned'}</span>
+                  <span>{exam.mainProfessor?.firstName} {exam.mainProfessor?.lastName || 'N/A'}</span>
                   <span>
                     {exam.secondaryProfessor
                       ? `${exam.secondaryProfessor.firstName} ${exam.secondaryProfessor.lastName}`
                       : 'N/A'}
-                  </span>  {/* Secondary Professor */}
-                  <span>{new Date(exam.date).toLocaleDateString()}</span>   {/* Exam Date */}
-                  <span>{exam.hour}</span>  {/* Exam Hour */}
-                  <span>{exam.classroom?.name || 'No classroom assigned'}</span>   {/* Classroom */}
+                  </span>
+                  <span>{new Date(exam.date).toLocaleDateString()}</span>
+                  <span>{exam.hour}</span>
+                  <span>{exam.classroom?.name || 'No classroom assigned'}</span>
                   {userRole === 'professor' && (
-                  <span>
-                    <ExamDropdown examId={exam._id} onDelete={removeExamFromState} />
-                  </span>)}
+                    <span>
+                      <ExamDropdown examId={exam._id} onDelete={removeExamFromState} />
+                    </span>
+                  )}
                 </div>
               ))
             ) : (
               <div>No exams found.</div>
             )}
           </div>
+
+          <ReactPaginate
+            previousLabel={<button className="btn btn-outline-primary">Previous</button>}
+            nextLabel={<button className="btn btn-outline-primary">Next</button>}
+            breakLabel={'...'}
+            pageCount={totalPages}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={3}
+            onPageChange={handlePageClick}
+            containerClassName="pagination justify-content-center mt-4"
+            activeClassName="active"
+            previousClassName={`page-item ${currentPage === 0 ? 'disabled' : ''}`}
+            nextClassName={`page-item ${currentPage === totalPages - 1 ? 'disabled' : ''}`}
+            pageClassName="page-item"
+            pageLinkClassName="page-link"
+            previousLinkClassName="page-link"
+            nextLinkClassName="page-link"
+          />
         </section>
       </main>
     </div>
